0c323b101471ce6184c263d213f0f71a00695196
Rahul Somaraj <rahul.tech@thenewshop.in>
2024-08-31 12:35:23 +0530

    add config variables for psql an dapp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import appConfig from "./config/app.config";
+import devConfig from "./config/dev.config";
+import stagConfig from "./config/stag.config";
+import { join } from "path";
+import * as os from "os";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			load: [appConfig, devConfig, stagConfig],
+			isGlobal: true,
+			envFilePath: join(os.homedir(), "ratingapp_backend", `.env`),
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,69 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as morgan from "morgan";
+import { AppModule } from "./app.module";
+import cors from "cors";
+
+process.env.TZ = "Asia/Kolkata";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	// Middleware
+	app.use(morgan("combined")); // Use the combined format
+
+	// CORS configuration
+	app.enableCors({
+		origin: "*", // Consider restricting this in production
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	});
+
+	const configService = app.get<ConfigService>(ConfigService);
+	const PORT = configService.get<number>("APP_CONFIG.PORT");
+
+	// Swagger setup
+	const config = new DocumentBuilder()
+		.setTitle("RatingApp Backend API")
+		.setDescription("RatingApp Backend API")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("docs", app, document);
+
+	// Global Validation Pipe
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true, // Automatically strip properties that do not have decorators
+			forbidNonWhitelisted: true, // Throw an error when non-whitelisted properties are present
+			transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+		})
+	);
+
+	// Handle unhandled promise rejections
+	process.on("unhandledRejection", (reason, promise) => {
+		console.error("Unhandled Promise Rejection:", reason);
+		// Optional: Integrate a logging service here for better tracking
+	});
+
+	process.on("SIGTERM", async () => {
+		Logger.log("SIGTERM signal received: closing HTTP server", "Bootstrap");
+		await app.close();
+	});
+
+	process.on("SIGINT", async () => {
+		Logger.log("SIGINT signal received: closing HTTP server", "Bootstrap");
+		await app.close();
+	});
+
+	// Start the application
+	await app.listen(PORT, () => {
+		Logger.log(`Server started at http://localhost:${PORT}/docs`, "Bootstrap");
+	});
 }
+
 bootstrap();
